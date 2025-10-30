5ee49d7a07cc7fce1dca49429f4e5bb7153fc5c7
Bedia09 <bediasatik@outlook.com>
2025-10-30 23:17:48 +0300

    Kategori.cs propertyler ve metod eklendi, UrunEkle metodu eklendi.

--- StokYonetimi/Kategori.cs
+++ StokYonetimi/Kategori.cs
@@ -8,5 +8,24 @@ namespace StokYonetimi
 {
     internal class Kategori
     {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+
+        public List<Urun> Urunler { get; set; }
+        public Kategori(int id, string ad)
+        {
+            Id = id;
+            Ad = ad;
+            Urunler = new List<Urun>();
+        }
+        public void UrunEkle (Urun urun)
+        {
+            Urunler.Add(urun);
+            Console.WriteLine(urun.Ad + "ürünü '" + Ad + "' kategorisine eklendi.");
+        }
+        public override string ToString()
+        {
+            return Id + " - " + Ad + " | Ürün Sayısı: " + Urunler.Count;
+        }
     }
 }
