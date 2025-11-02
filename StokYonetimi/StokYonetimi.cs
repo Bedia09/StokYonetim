9257c000ac853f21f992ad4716b7245cb508050d
Bedia09 <bediasatik@outlook.com>
2025-11-02 17:48:49 +0300

    Stokİslemleri.cs StokYonetimi.cs olarak değiştirildi. StokYonetimi.cs KategoriEkle metodu eklendi

---
+++ StokYonetimi/StokYonetimi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokYonetimi
+{
+    public class StokYonetimi
+    {
+        public List<Kategori> Kategoriler { get; set; }
+        public StokYonetimi()
+        {
+            Kategoriler = new List<Kategori>();
+        }
+        public void KategoriEkle (Kategori kategori)
+        {
+            Kategoriler.Add(kategori);
+            Console.WriteLine(kategori.Ad + "kategorsisi eklendi.");
+        }
+    }
+}
