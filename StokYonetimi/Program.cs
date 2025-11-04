59c4430f3650fe59dd0e24a49ad26ad710811558
Bedia09 <bediasatik@outlook.com>
2025-11-04 20:18:23 +0300

    Program.cs ürün oluşturuldu,kategoriye eklendi.

--- StokYonetimi/Program.cs
+++ StokYonetimi/Program.cs
@@ -1,7 +1,26 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokYonetimi
 {
-    private static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello, World!");
+         static void Main(string[] args)
+        {
+           StokYonetimi stok = new StokYonetimi();
+
+            Kategori kirtasiyeKategori = new Kategori(1, "Kırtasiye");
+            Urun kalem = new Urun(1, "Kalem", 20, 40);
+            Urun silgi = new Urun(2, "Silgi", 10, 30);
+            Urun defter = new Urun(3, "Defter", 15, 35);
+
+            kirtasiyeKategori.UrunEkle (kalem);
+            kirtasiyeKategori.UrunEkle (silgi);
+            kirtasiyeKategori.UrunEkle (defter);
+
+        }
     }
 }
